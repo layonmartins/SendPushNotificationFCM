07ced75a8063442b3c2b1decd7c8806121008edb
layonfonseca <layon.fonseca@eldorado.org.br>
2022-03-15 13:51:36 -0300

    Create resulta activity

--- app/src/main/java/com/layon/sendpushnotificationfcm/service/FirebaseMessagingService.kt
+++ app/src/main/java/com/layon/sendpushnotificationfcm/service/FirebaseMessagingService.kt
@@ -1,8 +1,10 @@
 package com.layon.sendpushnotificationfcm.service
 
+import android.content.Intent
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.layon.sendpushnotificationfcm.ResultActivity
 import com.layon.sendpushnotificationfcm.utils.NotificationUtil
 
 class FirebaseMessagingService : FirebaseMessagingService() {
@@ -18,12 +20,24 @@ class FirebaseMessagingService : FirebaseMessagingService() {
             "message.notification: ${remoteMessage.notification?.title} ${remoteMessage.notification?.body}"
         )
 
+        remoteMessage.data?.let {
+            Log.d("layon.f", "message.data.getdata: ${remoteMessage.data.get("data")}")
+            if (it.get("data").toString() == "open result") {
+                Log.d("layon.f", "it.contains(\"data\").toString() ${it.contains("data").toString()}")
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+
+
         remoteMessage.notification?.let { notification ->
             NotificationUtil.createNotification(
                 context = this,
                 title = notification.title.toString(),
                 body = notification.body.toString(),
-                data = remoteMessage.data)
+                data = remoteMessage.data
+            )
         }
     }
 
